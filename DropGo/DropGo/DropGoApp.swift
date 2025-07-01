115f9f4d6cd5969366d11442f6b5758c03b84740
JAEHYUN GO <gojaehyeonstudio@gmail.com>
2025-07-01 12:39:16 +0900

    feat: 프로젝트 생성 및 세팅

---
+++ DropGo/DropGo/DropGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropGoApp.swift
+//  DropGo
+//
+//  Created by Gojaehyun on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct DropGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DropGo/DropGoTests/DropGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropGoTests.swift
+//  DropGoTests
+//
+//  Created by Gojaehyun on 7/1/25.
+//
+
+import Testing
+@testable import DropGo
+
+struct DropGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
